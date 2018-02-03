970267aee070ca1396477085162d31bf3a682f12
Jose Ramirez <josecrmrz@gmail.com>
2018-02-03 00:18:06 -0600

    Add to score with buttons

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -2,12 +2,44 @@ package com.example.android.courtcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int scoreTeamA = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        displayForTeamA(0);
+    }
+
+    /**
+     * Displays the given score for Team A.
+     */
+    private void displayForTeamA(int score) {
+        TextView scoreView = findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    public void addThreeForTeamA(View view) {
+        scoreTeamA += 3;
+
+        displayForTeamA(scoreTeamA);
+    }
+
+    public void addTwoForTeamA(View view) {
+        scoreTeamA += 2;
+
+        displayForTeamA(scoreTeamA);
+    }
+
+    public void addFreeThrowForTeamA(View view) {
+        scoreTeamA += 1;
+
+        displayForTeamA(scoreTeamA);
     }
 }
